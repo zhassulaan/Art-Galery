7b4c29792097c48587f07972bb0db540311ec39c
Zhassulan <190103383@stu.sdu.edu.kz>
2023-07-28 10:16:36 +0600

    template

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import DefaultLayout from '../layouts/DefaultLayout.vue';
+import HomeLayout from '../layouts/HomeLayout.vue';
+import Home from '../views/Home.vue';
+
+const routes = [
+  {
+    path: '/',
+    component: HomeLayout,
+    children: [
+      { path: '', component: Home, name: 'Home' }
+    ],
+  },
+  {
+    path: '/details/:id',
+    component: DefaultLayout,
+    children: [
+      { path: '', component: () => import('../views/Detail.vue'), name: 'Detail' }
+    ],
+  },
+  {
+    path: '/favourites',
+    component: DefaultLayout,
+    children: [
+      { path: '', component: () => import('../views/Favourites.vue'), name: 'Favourites' }
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
