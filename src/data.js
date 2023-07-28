b13ef45101891ce60286088ae4750ce671524163
Zhassulan <190103383@stu.sdu.edu.kz>
2023-07-28 15:27:52 +0600

    details  page

--- src/data.js
+++ src/data.js
@@ -7,6 +7,13 @@ import image6 from './assets/images/image6.png';
 import image7 from './assets/images/image7.png';
 import image8 from './assets/images/image8.png';
 import image9 from './assets/images/image9.png';
+import avatar1 from './assets/images/avatar1.jpeg';
+import avatar2 from './assets/images/avatar2.jpeg';
+import avatar3 from './assets/images/avatar3.jpeg';
+import avatar4 from './assets/images/avatar4.jpeg';
+import avatar5 from './assets/images/avatar5.jpeg';
+import avatar6 from './assets/images/avatar6.jpeg';
+import avatar7 from './assets/images/avatar7.jpeg';
 
 export const images = [
 	{
@@ -15,6 +22,7 @@ export const images = [
 		name_en: 'And the light shines in the darkness',
 		artist: 'Александр Павлович Афонин',
 		artist_en: 'Alexander Pavlovich Afonin',
+		avatar: avatar1,
 		image: image1,
 	},
 	{
@@ -23,6 +31,7 @@ export const images = [
 		name_en: 'Storm Clouds',
 		artist: 'Орловский Владимир Донатович',
 		artist_en: 'Orlovsky Vladimir Donatovich',
+		avatar: avatar2,
 		image: image2,
 	},
 	{
@@ -31,6 +40,7 @@ export const images = [
 		name_en: 'Portrait of an Unknown Woman',
 		artist: 'Иван Николаевич Крамской',
 		artist_en: 'Ivan Nikolaevich Kramskoi',
+		avatar: avatar3,
 		image: image3,
 	},
 	{
@@ -39,6 +49,7 @@ export const images = [
 		name_en: 'Old Bridge in Tver',
 		artist: 'Юдин Александр Владимирович',
 		artist_en: 'Yudin Alexander Vladimirovich',
+		avatar: avatar4,
 		image: image4,
 	},
 	{
@@ -47,6 +58,7 @@ export const images = [
 		name_en: 'Ein Paar Schuhe',
 		artist: 'Винсент ван Гог',
 		artist_en: 'Vincent van Gogh',
+		avatar: avatar5,
 		image: image5,
 	},
 	{
@@ -55,6 +67,7 @@ export const images = [
 		name_en: 'Starry Night Over the Rhône',
 		artist: 'Винсент ван Гог',
 		artist_en: 'Vincent van Gogh',
+		avatar: avatar5,
 		image: image6,
 	},
 	{
@@ -63,6 +76,7 @@ export const images = [
 		name_en: 'Freshness of Cold',
 		artist: 'Афремов Леонид Аркадиевич',
 		artist_en: 'Afremov Leonid Arkadievich',
+		avatar: avatar6,
 		image: image7,
 	},
 	{
@@ -71,6 +85,7 @@ export const images = [
 		name_en: 'Mona Lisa',
 		artist: 'Леонардо да Винчи',
 		artist_en: 'Leonardo da Vinci',
+		avatar: avatar7,
 		image: image8,
 	},
 	{
@@ -79,6 +94,7 @@ export const images = [
 		name_en: 'Branches with Almond Blossom',
 		artist: 'Винсент ван Гог',
 		artist_en: 'Vincent van Gogh',
+		avatar: avatar5,
 		image: image9,
 	},
 ];
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,5 +1,32 @@
 <template>
-  <main class='container'>
-    <h1>Detail Page</h1>
+  <main class='detail container'>
+    <div class='detail-background'>
+      <img :src='images[0].image' class='detail-background__image' :alt='images[0].name'>
+    </div>
+    <div class='detail-header'>
+      <div class='detail-header__left'>
+        <img :src='images[0].avatar' class='detail-header__left-avatar' :alt='images[0].artist'>
+        <h6 class='detail-header__left-title'>{{ images[0].artist_en }}</h6>
+        <p class='detail-header__left-subtitle'>{{ `@${ images[0].artist_en.replace(/\s+/g, '').toLowerCase() }` }}</p>
+      </div>
+      <div class='detail-header__right'>
+        <icon-favourite />
+        <div class="detail-header__right-button">
+          <icon-download />
+          <p>Download</p>
+        </div>
+      </div>
+    </div>
+    <div class='detail-body'>
+      <img :src='images[0].image' class='detail-body__image' :alt='images[0].name'>
+      <icon-maximize class='detail-body__button' />
+    </div>
   </main>
 </template>
+
+<script setup>
+import { images } from '../data';
+import IconFavourite from '../components/icons/Favourite.vue';
+import IconDownload from '../components/icons/Download.vue';
+import IconMaximize from '../components/icons/Maximize.vue';
+</script>
