6ef760631fe8d0de51a7571f15df4cb5b3c5b77e
Zhassulan <190103383@stu.sdu.edu.kz>
2023-07-28 11:12:20 +0600

    header

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,5 +1,15 @@
 <template>
-	<header>
-		<h1>Header</h1>
+	<header class='header'>
+		<img src='@/assets/icons/logo.svg' class='header-logo' alt='logo' />
+		<ul class='header-menu'>
+			<li class='header-menu__item'>
+				<img src='@/assets/icons/search.svg' alt='search' />
+				<p>Поиск</p>
+			</li>
+			<li class='header-menu__item'>
+				<img src='@/assets/icons/heart.svg' alt='heart' />
+				<p>Избранное</p>
+			</li>
+		</ul>
 	</header>
 </template>
--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class='default-layout'>
+  <div class='home-layout'>
     <Header />
     <router-view />
   </div>
