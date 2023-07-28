b3ef3d4228ae937dc1a5f702eeaea4ec0dbd9fb9
Zhassulan <190103383@stu.sdu.edu.kz>
2023-07-28 14:15:28 +0600

    favourites page

--- src/views/Favourites.vue
+++ src/views/Favourites.vue
@@ -1,5 +1,14 @@
 <template>
-  <main class='container'>
-    <h1>Избранное</h1>
+  <main class='favourite container'>
+    <h1 class="favourite-title">Избранное</h1>
+    <div class='favourite-body'>
+      <div class='favourite-body__item' v-for='item in images' :key='item.id'>
+        <img :src='item.image' :alt='item.name' />
+      </div>
+    </div>
   </main>
 </template>
+
+<script setup>
+import { images } from '../data';
+</script>
